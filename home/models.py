c386e175e03b5ba69a58451041e9f30bb103c021
lasse-cs <lasse.schmieding@gmail.com>
2025-11-23 10:15:20 +0000

    Update HomePage model to add intro

--- home/models.py
+++ home/models.py
@@ -1,5 +1,20 @@
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
 from wagtail.models import Page
 
 
 class HomePage(Page):
+    intro = RichTextField(
+        help_text="Intro text for the homepage.",
+        default="Blog intro content.",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("intro"),
+    ]
+
+    parent_page_types = ["wagtailcore.Page"]
+
+    max_count = 1
+
     template = "patterns/pages/home/home_page.html"
