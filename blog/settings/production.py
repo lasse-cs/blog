92ed5116bbecedb041ead92d306d2220abeedce4
lasse-cs <lasse.schmieding@gmail.com>
2025-11-30 08:58:25 +0000

    Add a basic production Dockerfile

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -1,3 +1,6 @@
+# ruff: noqa: F403, F405
+import os
+
 from .base import *
 
 DEBUG = False
@@ -7,6 +10,9 @@ DEBUG = False
 # (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/5.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
+ALLOWED_HOSTS = os.environ["DJANGO_ALLOWED_HOSTS"].split(",")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 try:
     from .local import *
