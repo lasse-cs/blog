f671f2bf5aec13a37e179a8847e36cf938041160
lasse-cs <lasse.schmieding@gmail.com>
2025-12-06 11:01:02 +0000

    Switch to postgres in production!

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -1,4 +1,5 @@
 # ruff: noqa: F403, F405
+from pathlib import Path
 import os
 
 from .base import *
@@ -12,13 +13,24 @@ DEBUG = False
 STORAGES["staticfiles"]["BACKEND"] = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 ALLOWED_HOSTS = os.environ["DJANGO_ALLOWED_HOSTS"].split(",")
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = Path(os.environ["DJANGO_SECRET_KEY_FILE"]).read_text().strip()
 
 # Deployment settings
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PASSWORD": Path(os.environ["POSTGRES_PASSWORD_FILE"]).read_text().strip(),
+        "PORT": 5432,
+    }
+}
 
 try:
     from .local import *
