e74f679824f73572b7a1cf87cd91e4388387773e
lasse-cs <lasse.schmieding@gmail.com>
2025-12-08 21:32:37 +0000

    Add Social Sidebar block

--- src/core/blocks.py
+++ src/core/blocks.py
@@ -1,12 +1,45 @@
-from wagtail.blocks import CharBlock, RichTextBlock, StreamBlock, StructBlock
+from django.db import models
+
+from wagtail.blocks import (
+    CharBlock,
+    ChoiceBlock,
+    RichTextBlock,
+    StreamBlock,
+    StructBlock,
+    URLBlock,
+)
 
 
-class TitledTextBlock(StructBlock):
+class SocialMediaChoices(models.TextChoices):
+    GITHUB = "github", "GitHub"
+    BLUESKY = "bluesky", "BlueSky"
+    FEED = "feed", "Feed"
+
+
+class SocialLinkBlock(StructBlock):
+    display = CharBlock(
+        required=True,
+        help_text="The display text of this social media link",
+        max_length=60,
+    )
+    url = URLBlock(required=True, help_text="The URL of this social media link")
+    type = ChoiceBlock(
+        choices=SocialMediaChoices.choices,
+        default=SocialMediaChoices.GITHUB,
+        help_text="The type of social media link",
+        required=True,
+    )
+
+
+class BaseSidebarItemBlock(StructBlock):
     title = CharBlock(
         required=True,
         help_text="The title of this block.",
         max_length=60,
     )
+
+
+class TitledTextBlock(BaseSidebarItemBlock):
     text = RichTextBlock(
         required=True,
         help_text="The text of this block.",
@@ -14,10 +47,21 @@ class TitledTextBlock(StructBlock):
 
     class Meta:
         template = "patterns/components/sidebar/blocks/titled_text_block.html"
+        icon = "doc-full-inverse"
+
+
+class SocialBlock(BaseSidebarItemBlock):
+    class Meta:
+        template = "patterns/components/sidebar/blocks/social_block.html"
+        icon = "link-external"
 
 
 class SidebarBlock(StreamBlock):
     text = TitledTextBlock()
+    social = SocialBlock()
 
     class Meta:
         template = "patterns/components/sidebar/sidebar.html"
+        block_counts = {
+            "social": {"max_num": 1},
+        }
--- src/core/models.py
+++ src/core/models.py
@@ -5,8 +5,10 @@ from django.utils.feedgenerator import Atom1Feed
 from wagtail.admin.panels import FieldPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, path
 from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.search import index
+
+from core.blocks import SocialLinkBlock
 
 
 class BaseRSSFeed(Feed):
@@ -17,7 +19,7 @@ class BaseRSSFeed(Feed):
 
     def title(self, obj):
         return obj.feed_title()
-    
+
     def description(self, obj):
         return obj.feed_description
 
@@ -26,10 +28,10 @@ class BaseRSSFeed(Feed):
 
     def items(self, obj):
         return obj.get_feed_items()[:30]
-    
+
     def item_title(self, item):
         return item.feed_title
-    
+
     def item_link(self, item):
         return item.url
 
@@ -39,7 +41,7 @@ class BaseRSSFeed(Feed):
 
 class BaseAtomFeed(BaseRSSFeed):
     feed_type = Atom1Feed
-    
+
     def subtitle(self, obj):
         return self.descripion(obj)
 
@@ -63,13 +65,21 @@ class FeedMixin(RoutablePageMixin):
         return self.atom_feed_instance(request, self)
 
     def get_feed_items(self):
-        return self.get_children().type(FeedItemMixin).live().public().specific().order_by('-first_published_at')
+        return (
+            self.get_children()
+            .type(FeedItemMixin)
+            .live()
+            .public()
+            .specific()
+            .order_by("-first_published_at")
+        )
 
 
 class FeedItemMixin(models.Model):
     """
     Mixin to specify that a model can be included in a Feed.
     """
+
     intro = RichTextField(
         help_text="Intro text for the page, used also in summaries.",
         default="Intro content.",
@@ -97,4 +107,16 @@ class FeedItemMixin(models.Model):
 
 @register_setting
 class SiteFooter(BaseSiteSetting):
-    content = RichTextField("Footer Content", max_length=255, features=["italic", "link", "bold"])
+    content = RichTextField(
+        "Footer Content", max_length=255, features=["italic", "link", "bold"]
+    )
+
+
+@register_setting
+class SocialMediaLinks(BaseSiteSetting):
+    links = StreamField(
+        [
+            ("link", SocialLinkBlock()),
+        ],
+        help_text="Social Media Links",
+    )
