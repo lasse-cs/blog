5264108be7fdb8ba8aac32264e098daae958d870
lasse-cs <lasse.schmieding@gmail.com>
2025-12-31 09:47:28 +0000

    Improved SVG Handling for social links?

--- src/core/templatetags/custom_tags.py
+++ src/core/templatetags/custom_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.template.loader import render_to_string
 from django.utils.html import format_html
 
 register = template.Library()
@@ -15,3 +16,12 @@ def highlight_after_first_dot(value):
         return value
 
     return format_html('{}<span class="highlight">.{}</span>', split[0], split[1])
+
+
+@register.simple_tag
+def svg_icon(icon_name, class_name=None):
+    svg = render_to_string(f"non_patterns/svgs/{icon_name}.svg")
+    if class_name:
+        return format_html('<span class="{}">{}</span>', class_name, svg)
+    else:
+        return format_html("<span>{}</span>", svg)
