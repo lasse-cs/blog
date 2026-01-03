304fd60657a797833a5adfbf6e09926121c10633
lasse-cs <lasse.schmieding@gmail.com>
2026-01-03 22:16:43 +0000

    Add a transition for theme-switching

--- src/static_src/js/controllers/theme_controller.js
+++ src/static_src/js/controllers/theme_controller.js
@@ -1,20 +1,43 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    toggle() {
+    toggle(event) {
         const currentTheme = document.documentElement.getAttribute('data-theme') || "light";
-        this.setTheme(currentTheme === "dark" ? "light" : "dark");
+        this.setTheme(currentTheme === "dark" ? "light" : "dark", event);
     }
 
-    setTheme(theme) {
-        if (theme === "dark") {
-            document.documentElement.setAttribute("data-theme", theme);
+    setTheme(theme, event) {
+        if (!document.startViewTransition) {
+            this.updateTheme(theme);
         } else {
-            document.documentElement.removeAttribute("data-theme");
+            const rect = event.target.getBoundingClientRect();
+
+            const x = rect.left + rect.width / 2;
+            const y = rect.top + rect.height / 2;
+
+            const xPercent = (x / window.innerWidth) * 100;
+            const yPercent = (y / window.innerHeight) * 100;
+
+            document.documentElement.style.setProperty("--x", `${xPercent}%`);
+            document.documentElement.style.setProperty("--y", `${yPercent}%`);
+
+            document.documentElement.style.viewTransitionName = "theme-switch";
+            const transition = document.startViewTransition(() => {
+                this.updateTheme(theme);
+            })
+            transition.finished.finally(() => document.documentElement.style.viewTransitionName = "");
         }
 
         if (localStorage) {
             localStorage.setItem("theme", theme);
+        }
+    }
+
+    updateTheme(theme) {
+        if (theme === "dark") {
+            document.documentElement.setAttribute("data-theme", theme);
+        } else {
+            document.documentElement.removeAttribute("data-theme");
         }
     }
 }
