e286be76cacfbb56ce719f5d57f28869b9c5ee25
lasse-cs <lasse.schmieding@gmail.com>
2026-01-04 10:33:00 +0000

    More deploy scripts, add a launcher

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Docker Build"
+docker compose build django
+docker compose build nginx
+
+echo "Recreate containers"
+docker compose up -d
+
+if [ "${MIGRATE:-0}" = 1 ]; then
+    echo "Running migrations"
+    sleep 5
+    docker compose exec django python manage.py migrate --noinput
+fi
+
+echo "Deployment complete!"
