e815a5e8e52a3410a260c84401389f5ebdb83e02
lasse-cs <lasse.schmieding@gmail.com>
2026-01-04 10:11:49 +0000

    Add a simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+echo "Fetching source"
+git fetch origin main
+git reset --hard origin/main
+
+echo "Docker Build"
+docker compose build
+
+echo "Recreate containers"
+docker compose up -d
+
+if [ "${MIGRATE:-0}" = 1 ]; then
+    echo "Running migrations"
+    sleep 5
+    docker compose exec django python manage.py migrate --noinput
+fi
+
+echo "Deployment complete!"
