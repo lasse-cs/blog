bca4deb9a3a56acb9b2a58d2a507199ac4e774a9
lasse-cs <lasse.schmieding@gmail.com>
2026-05-17 14:26:52 +0100

    Deploy script update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,12 +6,12 @@ docker compose -f compose.yaml -f compose.proxy.yaml build django
 docker compose -f compose.yaml -f compose.proxy.yaml build nginx
 
 echo "Recreate containers"
-docker compose down && docker compose -f compose.yaml -f compose.proxy.yaml up -d
+docker compose -f compose.yaml -f compose.proxy.yaml down && docker compose -f compose.yaml -f compose.proxy.yaml up -d
 
 if [ "${MIGRATE:-0}" = 1 ]; then
     echo "Running migrations"
     sleep 5
-    docker compose exec django python manage.py migrate --noinput
+    docker compose -f compose.yaml -f compose.proxy.yaml exec django python manage.py migrate --noinput
 fi
 
 echo "Deployment complete!"
